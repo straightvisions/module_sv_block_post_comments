0e805e2677ec539ff67bfb99c7b1961d8777229b
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-06-21 17:48:24 +0200

    improve scripts handling

--- sv_block_post_comments.php
+++ sv_block_post_comments.php
@@ -60,19 +60,4 @@
 
 			return $this;
 		}
-		public function enqueue_scripts(): sv_block_post_comments {
-			if(!$this->has_block_frontend('post-comments')){
-				return $this;
-			}
-
-			if(!is_admin()){
-				$this->load_settings()->register_scripts();
-			}
-
-			foreach($this->get_scripts() as $script){
-				$script->set_is_enqueued();
-			}
-			
-			return $this;
-		}
 	}
